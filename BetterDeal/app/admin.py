b8e9c31c130aaf6b48d4aaa9bc7c8df24e17c48c
Korpa Bence <korpabence98@gmail.com>
2021-10-20 17:06:45 +0200

    empty DummyDatabase

--- BetterDeal/app/admin.py
+++ BetterDeal/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import DummyDatabase
+
+class DummyAdmin(admin.ModelAdmin):
+	list_display=('name','price',)
+
+admin.site.register(DummyDatabase,DummyAdmin)
--- BetterDeal/app/models.py
+++ BetterDeal/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class DummyDatabase(models.Model):
+    name= models.CharField(max_length=255)
+    price=models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Dummies"
+
+    def __str__(self):
+        return (self.name + " " + self.price)
+
+
+
+	
