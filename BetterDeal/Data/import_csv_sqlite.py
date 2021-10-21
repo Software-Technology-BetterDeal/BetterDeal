bfe81f086ded11f6508609d478507135ff3525f4
unknown <selma.mhenni@gmail.com>
2021-10-21 19:01:05 +0200

    adding tesco data script & import into sqlite

---
+++ BetterDeal/Data/import_csv_sqlite.py
@@ -0,0 +1,16 @@
+import csv, sqlite3
+
+con = sqlite3.connect("..\db.sqlite3")
+
+
+cur = con.cursor()
+cur.execute("CREATE TABLE product (product_name, price, supermarket);") 
+
+with open('tesco_discounts.csv','r') as fin: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [(i['product_name'], i['price'], i['supermarket']) for i in dr]
+
+cur.executemany("INSERT INTO product (product_name, price, supermarket) VALUES (?, ?, ?);", to_db)
+con.commit()
+con.close()
