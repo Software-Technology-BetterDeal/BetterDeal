ea987220c277d45faad871eeb3422d39f5b372ea
Korpa Bence <korpabence98@gmail.com>
2021-10-24 23:50:19 +0200

    database with profile, cart

--- BetterDeal/app/models.py
+++ BetterDeal/app/models.py
@@ -1,26 +1,12 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from django.db.models import Q
 
 # Create your models here.
-
-class DummyDatabase(models.Model):
-    name= models.CharField(max_length=255)
-    price=models.IntegerField()
-
-
-    class Meta:
-        verbose_name_plural = "Dummies"
-
-    def __str__(self):
-        return (self.name + " " + str(self.price))
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        object_list = DummyDatabase.objects.filter(
-            Q(name__icontains=query) 
-        )
-        return object_list
 
 
 class Product (models.Model):
@@ -41,3 +27,26 @@ class Product (models.Model):
             Q(product_name__icontains=query) 
         )
         return object_list
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    cart=models.ManyToManyField(Product)
+
+    class Meta:
+        db_table = "profile"
+
+    def __str__(self):
+        return self.username
+
+    def get_cart(self):
+        return list(self.cart.all())
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
