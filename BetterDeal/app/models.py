73e030f7942513e544f5b51152b15f860c77a768
Cecilia Horvath <uch4kk@inf.elte.hu>
2021-11-10 13:33:11 +0100

    Tests: product.__str__(), profile.__str__()

--- BetterDeal/app/models.py
+++ BetterDeal/app/models.py
@@ -6,47 +6,55 @@ from django.dispatch import receiver
 
 from django.db.models import Q
 
+
 # Create your models here.
 
 
-class Product (models.Model):
-    product_name=models.CharField(max_length=255)
-    price=models.IntegerField()
-    supermarket=models.CharField(max_length=255)
-    image_name=models.CharField(max_length=255)
-
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    supermarket = models.CharField(max_length=255)
+    image_name = models.CharField(max_length=255)
+    """
+    def __init__(self, product_name, price, supermarket, image_name):
+        self.product_name = product_name
+        self.price = price
+        self.supermarket = supermarket
+        self.image_name = image_name
+    """
     class Meta:
         db_table = "product"
 
     def __str__(self):
-        return (self.product_name + " " + str(self.price) + " " + self.supermarket+ " "+ self.image_name)
-
+        return (self.product_name + " " + str(self.price) + " " + self.supermarket + " " + self.image_name)
 
     def get_queryset(self):
         query = self.request.GET.get('q')
         object_list = Product.objects.filter(
-            Q(product_name__icontains=query) 
+            Q(product_name__icontains=query)
         )
         return object_list
 
 
 class Profile(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE)
-    cart=models.ManyToManyField(Product)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cart = models.ManyToManyField(Product)
 
     class Meta:
         db_table = "profile"
 
     def __str__(self):
-        return self.username 
+        return self.user.username
 
     def get_cart(self):
         return list(self.cart.all())
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
--- BetterDeal/app/tests.py
+++ BetterDeal/app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from .models import *
+from django.contrib.auth.models import AnonymousUser, User
+
 
 # Create your tests here.
+
+class ProductModelTests(TestCase):
+
+    def test___str__(self):
+        product = Product(product_name='tej', price=230, supermarket='spar', image_name='tej.jpg')
+        result = product.__str__()
+        self.assertEqual(product.price, 230)
+        self.assertEqual(result, 'tej 230 spar tej.jpg')
+
+
+class ProfileModelTests(TestCase):
+
+    def test___str__(self):
+        user1 = User.objects.create_user('user', 'user@gmail.com', 'pw')
+        profile = Profile(user=user1)
+        result = profile.__str__()
+        self.assertEqual(result, 'user')
+
+
+
