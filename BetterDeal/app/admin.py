3626398846eccd8494328664b55cfbbbf7077dbd
Cecilia Horvath <uch4kk@inf.elte.hu>
2021-12-07 12:58:03 +0100

    Reformat admin.py

--- BetterDeal/app/admin.py
+++ BetterDeal/app/admin.py
@@ -5,11 +5,14 @@ from django.contrib import admin
 from .models import Product
 from .models import Profile
 
+
 class ProductAdmin(admin.ModelAdmin):
-	list_display=('product_name','price','supermarket',)
+    list_display = ('product_name', 'price', 'supermarket',)
+
 
 class ProfileAdmin(admin.ModelAdmin):
-	list_display=('user',)
+    list_display = ('user',)
 
-admin.site.register(Profile,ProfileAdmin)
-admin.site.register(Product,ProductAdmin)
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Product, ProductAdmin)
